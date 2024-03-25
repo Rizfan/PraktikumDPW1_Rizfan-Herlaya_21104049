babe0e2e4d56682277634372b26c578497971320
Rizfan <rispanherlaya@gmail.com>
2024-03-25 13:48:16 +0700

    latihan php

--- pertemuan3/belajar.php
+++ pertemuan3/belajar.php
@@ -18,10 +18,17 @@
 // define("nama", "Kang Daniel");
 // echo nama;
 
-function tambah($a, $b)
+// function tambah($a, $b)
+// {
+//     $total = $a + $b;
+//     return $total;
+// }
+// $total = tambah(10, 5);
+// echo $total;
+
+function bilangan_prima()
 {
-    $total = $a + $b;
-    return $total;
+    echo "cari sendiri";
 }
-$total = tambah(10, 5);
-echo $total;
+
+bilangan_prima();
