4538ef3d4643e3fbe88825d8a56ea9f71164894e
Rizfan <rispanherlaya@gmail.com>
2024-03-25 13:46:59 +0700

    Latihan PHP

---
+++ pertemuan3/belajar.php
@@ -0,0 +1,27 @@
+<?php
+
+// echo "Saya sedang belajar PHP";
+// echo "<br>";
+// echo "Hello World!";
+
+// $a = "Saya sedang belajar PHP ";
+// $b = 7;
+// print $a;
+// echo $b;
+
+// print("hello world!");
+// echo "hello world";
+
+// const nama = "Kang Daniel";
+// echo nama;
+
+// define("nama", "Kang Daniel");
+// echo nama;
+
+function tambah($a, $b)
+{
+    $total = $a + $b;
+    return $total;
+}
+$total = tambah(10, 5);
+echo $total;
